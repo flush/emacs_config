294e9a059c8edd0f44d73cbfefe17ed34b300dec
Flushaas <flushaas@gmail.com>
2023-10-11 06:54:02 +0200

    eliminadas barras varias

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,6 +38,16 @@
  (setq inhibit-startup-message t)
  (global-display-line-numbers-mode)
 
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)          ; Disable the menu bar
+
+
+
 ;; =================
 ;; Autocompletado Shell
 ;; =================
