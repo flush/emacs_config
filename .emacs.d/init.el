0b2ae94220becae2726fb93db7f07e3507ccef2c
Flushaas <flushaas@gmail.com>
2023-10-15 23:43:24 +0200

    Configuración para shell script

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,10 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
 (package-initialize)
 
 (custom-set-variables
@@ -10,22 +12,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+ '(backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
  '(custom-enabled-themes '(tango-dark))
  '(package-selected-packages
-   '(multiple-cursors elpy projectile use-package yasnippet flycheck blacken )))
+   '(company-shell multiple-cursors projectile use-package yasnippet flycheck)))
 
 ;; =================
 ;; ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
-(custom-set-variables
-  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
-  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
 
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
 
 
-
-;; =================
 ;; flycheck
 ;; ================
 
@@ -37,27 +37,16 @@
  ;; ====================
  (setq inhibit-startup-message t)
  (global-display-line-numbers-mode)
- 
- ;; =====================
- ;; PYTHON
-;; ====================
 
-(set-language-environment "UTF-8")
-(use-package elpy
-  :ensure t
-  :defer t
-  :init
-  (advice-add 'python-mode :before 'elpy-enable))
-
-(elpy-enable)
-;; Enable flycheck for python
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-
-(use-package blacken
+;; =================
+;; Autocompletado Shell
+;; =================
+(use-package company-shell
   :ensure t)
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+(add-to-list 'company-backends 'company-shell)
 
 ;; ====================
 ;; YASNIPPET
@@ -65,7 +54,6 @@
 (use-package yasnippet
   :ensure t 
   )
-
 
 
 ;; =====================
@@ -87,3 +75,9 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
